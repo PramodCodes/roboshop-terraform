edef2d511abfab4d23da985bdb28b4412784ecc3
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2024-12-23 19:43:15 +0530

    update

---
+++ 01-vpc/main.tf
@@ -0,0 +1,15 @@
+module "roboshop" {
+    # source = "../tarraform-aws-vpc"
+    # changing source to git
+    source = "git::https://github.com/Pramodcodes/tarraform-aws-vpc.git?ref=main"
+    project_name = var.project_name
+    project_environment = var.project_environment
+    common_tags = var.common_tags
+    vpc_tags = var.vpc_tags
+    
+    # subnet creation
+    public_subnet_cidr = var.public_subnet_cidr
+    private_subnet_cidr = var.private_subnet_cidr
+    database_subnet_cidr = var.database_subnet_cidr
+    is_peering_required = var.is_peering_required
+}
---
+++ 01-vpc/variables.tf
@@ -0,0 +1,56 @@
+variable "robobshop_vpc_cidr" {
+    default = "10.0.0.0/16"
+    type = string
+}
+
+variable "common_tags" {
+    default = {
+        Project = "roboshop"
+        Environment = "dev"
+        Terraform = true
+    }
+}
+
+variable "vpc_tags" {
+    default = {}
+}
+
+variable "project_name" {
+    default = "roboshop"
+    type = string
+}
+
+variable "project_environment" {
+    default = "dev"
+    type = string
+}
+
+variable "public_subnet_cidr" {
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+}
+
+variable "private_subnet_cidr" {
+  default = [
+    "10.0.11.0/24",
+    "10.0.12.0/24"
+  ]
+}
+variable "database_subnet_cidr" {
+  default = [
+    "10.0.21.0/24",
+    "10.0.22.0/24"
+  ]
+}
+
+variable "is_peering_required" {
+  default = true
+  type = bool
+}
+
+variable "environment" {
+    default = "dev"
+    type = string
+}
