ee10ee82c674eeeee3efdcd4232e69f4ba31cf4c
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2024-12-25 20:15:09 +0530

    Update ec2-provision.sh

--- 04-ec2/ec2-provision.sh
+++ 04-ec2/ec2-provision.sh
@@ -5,11 +5,21 @@ cd /tmp
 git clone https://github.com/PramodCodes/roboshop-ansible-roles.git
 cd roboshop-ansible-roles
 echo pwd
-# Array of components
-components=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
+# # Array of components
+# components=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
 
-# Loop through each component and run the playbook
-for component in "${components[@]}"
-do
-    ansible-playbook -e component=$component main.yaml
-done
+# # Loop through each component and run the playbook
+# for component in "${components[@]}"
+# do
+#     ansible-playbook -e component=$component main.yaml
+# done
+ansible-playbook -e component=mongodb main.yaml
+ansible-playbook -e component=redis main.yaml
+ansible-playbook -e component=rabbitmq main.yaml
+ansible-playbook -e component=mysql main.yaml
+ansible-playbook -e component=catalogue main.yaml
+ansible-playbook -e component=user main.yaml
+ansible-playbook -e component=cart main.yaml
+ansible-playbook -e component=shipping main.yaml
+ansible-playbook -e component=payment main.yaml
+ansible-playbook -e component=web main.yaml
