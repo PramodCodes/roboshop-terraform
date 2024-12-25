120c20d7f08a1f4bc82673539ed83a7d0dbfbebb
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2024-12-25 17:46:39 +0530

    14:39

--- 04-ec2/data.tf
+++ 04-ec2/data.tf
@@ -18,13 +18,55 @@ data "aws_ami" "centos8" {
     }
 }
 
+# reading the security group ids from ssm for db instances
 data "aws_ssm_parameter" "mongodb_sg_id" {
   name = "/${var.project_name}/${var.environment}/mongodb_sg_id"
 }
+data "aws_ssm_parameter" "redis_sg_id" {
+  name = "/${var.project_name}/${var.environment}/redis_sg_id"
+}
+data "aws_ssm_parameter" "mysql_sg_id" {
+  name = "/${var.project_name}/${var.environment}/mysql_sg_id"
+}
+data "aws_ssm_parameter" "rabbitmq_sg_id" {
+  name = "/${var.project_name}/${var.environment}/rabbitmq_sg_id"
+}
 
-# we will get 2 subnets because of HA , so we will use 1a , to get the 1a from list we use slice function 
+# reading the subnet ids from ssm for microservices instances
+data "aws_ssm_parameter" "catalogue_sg_id" {
+  name = "/${var.project_name}/${var.environment}/catalogue_sg_id"
+}
+data "aws_ssm_parameter" "user_sg_id" {
+  name = "/${var.project_name}/${var.environment}/user_sg_id"
+}
+data "aws_ssm_parameter" "cart_sg_id" {
+  name = "/${var.project_name}/${var.environment}/cart_sg_id"
+}
+data "aws_ssm_parameter" "shipping_sg_id" {
+  name = "/${var.project_name}/${var.environment}/shipping_sg_id"
+}
+data "aws_ssm_parameter" "payment_sg_id" {
+  name = "/${var.project_name}/${var.environment}/payment_sg_id"
+}
+
+# reading the subnet ids from ssm for web instances
+data "aws_ssm_parameter" "web_sg_id" {
+  name = "/${var.project_name}/${var.environment}/web_sg_id"
+}
+
+
+
 # we will process this in locals
 data "aws_ssm_parameter" "database_subnets_ids" {
   name = "/${var.project_name}/${var.environment}/database_subnets_ids"
+}
+
+data "aws_ssm_parameter" "private_subnets_ids" {
+  name = "/${var.project_name}/${var.environment}/private_subnets_ids"
+}
+
+
+data "aws_ssm_parameter" "public_subnets_ids" {
+  name = "/${var.project_name}/${var.environment}/public_subnets_ids"
 }
 
--- 04-ec2/ec2.tf
+++ 04-ec2/ec2.tf
@@ -1,3 +1,4 @@
+# instances creation for db tier
 module "mongodb" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   ami = data.aws_ami.centos8.id
@@ -15,5 +16,81 @@ module "mongodb" {
         Componenet = "mongodb"
     },{
         Name = "${local.ec2_name}-mongodb"
+    })
+}
+
+module "redis" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.centos8.id
+
+  name = "${local.ec2_name}-redis"
+
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.redis_sg_id.value]
+  subnet_id              = local.database_subnet_id
+
+  tags = merge(
+    var.common_tags,
+    {
+        Componenet = "redis"
+    },{
+        Name = "${local.ec2_name}-redis"
+    })
+}
+
+module "mysql" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.centos8.id
+
+  name = "${local.ec2_name}-mysql"
+
+  instance_type          = "t3.small"
+  vpc_security_group_ids = [data.aws_ssm_parameter.mysql_sg_id.value]
+  subnet_id              = local.database_subnet_id
+
+  tags = merge(
+    var.common_tags,
+    {
+        Componenet = "mysql"
+    },{
+        Name = "${local.ec2_name}-mysql"
+    })
+}
+module "rabbitmq" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.centos8.id
+
+  name = "${local.ec2_name}-rabbitmq"
+
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.rabbitmq_sg_id.value]
+  subnet_id              = local.database_subnet_id
+
+  tags = merge(
+    var.common_tags,
+    {
+        Componenet = "rabbitmq"
+    },{
+        Name = "${local.ec2_name}-rabbitmq"
+    })
+}
+
+# microservices tier
+module "catalogue" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.centos8.id
+
+  name = "${local.ec2_name}-catalogue"
+
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
+  subnet_id              = local.database_subnet_id
+
+  tags = merge(
+    var.common_tags,
+    {
+        Componenet = "catalogue"
+    },{
+        Name = "${local.ec2_name}-catalogue"
     })
 }
